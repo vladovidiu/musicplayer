d66f10a09b8cd9b5d7d35e5a413aae418ae7b5dd
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2015-06-12 18:21:04 +0100

    Yeoman generator files

---
+++ src/app/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('musicPlayer')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('musicPlayer', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ui.router', 'ngMaterial'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
